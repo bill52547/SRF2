bfe992dbb31205afb357a196a5e633dc1033ac01
Minghao <mh.guo0111@gmail.com>
2018-12-16 23:41:26 +0800

    clean env

---
+++ srf2/test/operators/test_operator.py
@@ -0,0 +1,16 @@
+# encoding: utf-8
+'''
+@author: Minghao Guo, Xiang Hong, Gaoyu Chen and Weijie Tao
+@license: LGPL_v3.0
+@contact: mh.guo0111@gmail.com
+@software: srf_v2
+@file: test_operator.py
+@date: 12/16/2018
+@desc: new version of Scalable Reconstruction Framework for Medical Imaging
+'''
+from unittest import TestCase
+
+
+class Test_function_signature(TestCase):
+    def test__function_signature(self):
+        self.fail()
